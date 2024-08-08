b089f5e4559d636271a14f809a05ce8c072e9f84
pao-com-manteiga <pao.com.manteiga0@outlook.com>
2024-08-07 22:31:25 -0300

    Update userscript.js

--- mods/userscript.js
+++ mods/userscript.js
@@ -1,38 +1,15 @@
 (function() {
     'use strict';
-
-    const script = document.createElement('script');
-    script.src = 'https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js';
-    script.onload = function() {
-        initSpatialNavigation();
-    };
-    document.head.appendChild(script);
-
-    function initSpatialNavigation() {
-        const focusableElements = document.querySelectorAll('a, .focusable, div.flex, input[type=text], div.flare-light');
-
-        focusableElements.forEach(element => {
-            element.addEventListener('focus', () => {
-                element.style.outline = '2px solid red';
-                element.style.border = '2px solid red';
-            });
-
-            element.addEventListener('blur', () => {
-                element.style.outline = '';
-                element.style.border = '';
-            });
-        });
-
-        window.SpatialNavigation.init();
-        focusableElements.forEach(element => {
-            window.SpatialNavigation.add(element);
-        });
-
-        if (focusableElements.length > 0) {
-            focusableElements[0].focus();
-        }
-    }
+	
+	const script = document.createElement('script');
+	script.src = 'https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js';
+	script.onload = function() {
+		window.SpatialNavigation.init();
+		window.SpatialNavigation.makeFocusable();
+		window.SpatialNavigation.add({
+			selector: 'a, .focusable'
+		  });
+		window.SpatialNavigation.focus();
+	};
+	document.head.appendChild(script);
 })();
-
-
-
